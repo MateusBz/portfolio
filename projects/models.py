7119a1b96f1411cc0b45965fbaaac792b50b90c9
mateusz <mateusz.bzdzion@gmail.com>
2020-09-19 16:07:31 +0200

    create projects app

---
+++ projects/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=50)
+    github = models.URLField(max_length=250)
+    demo = models.URLField(max_length=250)
+
