ee1537a5f67c26d77847d5ecfddfe27d9b895f09
mateusz <mateusz.bzdzion@gmail.com>
2020-09-19 20:30:35 +0200

    create project detail view

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from projects.views import ProjectListView
+from projects.views import ProjectListView, ProjectDetailView
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='projects_list'),
+    path('', ProjectListView.as_view(), name='project_list'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from projects.models import Project
 
 class ProjectListView(ListView):
     model= Project
+    context_object_name = 'projects'
+
+class ProjectDetailView(DetailView):
+    model = Project
