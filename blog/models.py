ca5d4906b8e24c7bc03519f0fc56e88e8107462d
mateusz <mateusz.bzdzion@gmail.com>
2020-09-21 16:16:05 +0200

    create blog app

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    category = models.ManyToManyField('Category', related_name='posts') 
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView, DetailView
+
+from .models import Post, Category
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+    
