6aaa5b5fdd39c90eb0d278335f6453707d4e6162
mateusz <mateusz.bzdzion@gmail.com>
2020-09-22 20:15:25 +0200

    heroku deploy 9

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'von(1g(4-00hajis^s4cc2#0!y7)&zf&koaax4@4k-38h&u6g&'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
