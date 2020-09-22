255156dbb9b00300a39e32efa18342214844ee24
mateusz <mateusz.bzdzion@gmail.com>
2020-09-22 12:09:44 +0200

    heroku deploy

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
+    summary = models.CharField(max_length=250)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
