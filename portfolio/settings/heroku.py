726066cc691b446302fa17ca0d6fde58fc784988
mateusz <mateusz.bzdzion@gmail.com>
2020-09-30 20:59:16 +0200

    heroku 12

---
+++ portfolio/settings/heroku.py
@@ -0,0 +1,15 @@
+import environ
+
+from portfolio.settings.base import *
+
+env = environ.Env(DEBUG=(bool, False))
+
+DEBUG = env('DEBUG')
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': env.db(),
+}
