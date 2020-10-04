274a28e126433c967422eaa5f8e0450eef0d1297
mateusz <mateusz.bzdzion@gmail.com>
2020-10-04 18:12:31 +0200

    create blog category list

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView, AboutMeView
+from .views import PostListView, PostDetailView, AboutMeView, CategoryPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('about/', AboutMeView.as_view(), name='about_me'),
+    path('<category>/', CategoryPostListView.as_view(), name='post_category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import get_object_or_404
 
 from .models import Post, Category
 
@@ -14,4 +15,18 @@ class PostDetailView(DetailView):
 class AboutMeView(TemplateView):
     template_name = 'blog/about.html'
 
- 
+
+class CategoryPostListView(ListView):
+    template_name = 'blog/category_list.html'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, name=self.kwargs['category'])
+        return Post.objects.filter(categories=self.category)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.category
+        return context
+
+        
+    context_object_name = 'posts'
